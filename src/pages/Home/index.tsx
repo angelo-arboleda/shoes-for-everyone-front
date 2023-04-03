85d73b8261c6ec73609e23a8917f663a52b7e4f4
Angelo Arboleda <angeloarboleda33@gmail.com>
2023-04-03 20:38:53 -0300

    atlz

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import Button from "../../components/Button";
+import React from "react";
+
+const Home = () => {
+return <div className="h-[1000px] text-red-900">Home<Button size="small">Click</Button></div>;
+};
+
+
+export default Home;
